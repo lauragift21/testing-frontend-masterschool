729ab5e087dcc01cb599321d300930858183be80
lauragift21 <laurandidi21@gmail.com>
2023-05-09 13:09:56 +0200

    setup tests

---
+++ __tests__/header.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../src/app/header';
+
+describe('Header', () => {
+  it('renders without crashing', () => {
+    render(<Header />);
+    expect(screen.getByText('Hacker News Clone')).toBeInTheDocument();
+  });
+}
+);
